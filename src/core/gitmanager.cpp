9e6d6c1e62f70b81cc515ee7f862f1175cc4bce4
Joimar <joimarbgf@gmail.com>
2025-08-23 20:51:41 -0300

    executeGitCommand() implemented
    Method in GitManager implemented

--- src/core/gitmanager.cpp
+++ src/core/gitmanager.cpp
@@ -16,6 +16,8 @@ bool GitManager::setRepository(const QString &path)
     return true;
 }
 
+// Slot Functions___________________________________
+
 void GitManager::onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
 {
 
@@ -24,4 +26,38 @@ void GitManager::onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus
 void GitManager::onProcessError(QProcess::ProcessError)
 {
 
+}
+
+//__________________________________________________
+
+
+// Signal Functions________________________________
+
+QString GitManager::executeGitCommand(const QStringList &arguments)
+{
+    QProcess process;
+    process.setWorkingDirectory(m_repositoryPath);
+
+    // Define environment if necessary
+    QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
+    process.setProcessEnvironment(env);
+
+    // Execute command
+    process.start("git", arguments);
+
+    // Waits 30 secs by using timeout
+    if(!process.waitForFinished(30000))
+    {
+        process.kill();
+        return QString();
+    }
+
+    if(process.exitCode() != 0)
+    {
+        QString error = process.readAllStandardError();
+        emit commandError(error);
+        return QString();
+    }
+
+    return process.readAllStandardOutput();
 }
