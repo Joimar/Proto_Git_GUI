5a25c2bd82fd4ddc8be12cac9da72eb356d784d5
Joimar <joimarbgf@gmail.com>
2025-08-23 15:27:20 -0300

    git Manager with slots functions not fully implemented

--- src/core/gitmanager.cpp
+++ src/core/gitmanager.cpp
@@ -1,3 +1,27 @@
 #include "gitmanager.h"
 
-GitManager::GitManager(QObject *parent) {}
+#include <QDir>
+
+GitManager::GitManager(QObject *parent) : QObject(parent), m_currentProcess(nullptr)
+{
+
+}
+
+bool GitManager::setRepository(const QString &path)
+{
+    QDir dir(path);
+    if(!dir.exists() || !dir.exists(".git")) return false;
+
+    m_repositoryPath = path;
+    return true;
+}
+
+void GitManager::onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+
+}
+
+void GitManager::onProcessError(QProcess::ProcessError)
+{
+
+}
